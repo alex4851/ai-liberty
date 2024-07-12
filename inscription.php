9c08eb201856f745c21cce1b061b9a494469c9a0
alex4851 <alexisbeligne@gmail.com>
2024-07-12 17:19:28 +0200

    inscription et connexion form

--- inscription.php
+++ inscription.php
@@ -44,35 +44,46 @@
     </nav>
 </header>
 
-
-
 <div class="content">
-<section class="inscription">
-    <div class="formulaire_inscription">
-        <form  method="post" action="inscription.php">
-            <h2>S'inscrire</h2>
+<div class="inscription">
+        <form class="form"  method="post" action="inscription.php">
+        <p id="heading">S'inscrire</p>
+            <div class="field">
+                <input type="text" class="input-field" placeholder="Entrez votre prenom ..." id="prenom" name="prenom" required> <br/>
+            </div>
+            <div class="field">
+                <select name="niveau" id="niveau" class="input-field">
+                    <option value="lyceen">Lyceen</option>
+                    <option value="etudient">Etudient</option>
+                    <option value="professionnel">Professionnel</option>
+                </select> <br />
+            </div>
 
-            <label for="prenom">Votre prenom : *</label>
-            <input type="text" placeholder="Entrez votre prenom ..." id="prenom" name="prenom" required> <br/>
-            
-            <label for="niveau">Niveau : </label>
-            <select name="niveau" id="niveau">
-                <option value="lyceen">Lyceen</option>
-                <option value="etudient">Etudient</option>
-                <option value="professionnel">Professionnel</option>
-            </select> <br />
-
-            <label for="email">Votre email : *</label>
-            <input type="email" placeholder="Entrez votre email ..." id="email" name="email" required> <br />
-
-            <label for="pass">Votre mot de passe : *</label>
-            <input type="password" placeholder="Entrez votre mot de passe ..." id="pass" name="pass" required> <br />
-            
-            <label for="pass2">Confirmer mot de passe : *</label>
-            <input type="password" placeholder="Confirmer votre mot de passe ..." id="pass2" name="pass2" required> <br />
+            <div class="field">
+                <svg class="input-icon" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+                    <path d="M13.106 7.222c0-2.967-2.249-5.032-5.482-5.032-3.35 0-5.646 2.318-5.646 5.702 0 3.493 2.235 5.708 5.762 5.708.862 0 1.689-.123 2.304-.335v-.862c-.43.199-1.354.328-2.29.328-2.926 0-4.813-1.88-4.813-4.798 0-2.844 1.921-4.881 4.594-4.881 2.735 0 4.608 1.688 4.608 4.156 0 1.682-.554 2.769-1.416 2.769-.492 0-.772-.28-.772-.76V5.206H8.923v.834h-.11c-.266-.595-.881-.964-1.6-.964-1.4 0-2.378 1.162-2.378 2.823 0 1.737.957 2.906 2.379 2.906.8 0 1.415-.39 1.709-1.087h.11c.081.67.703 1.148 1.503 1.148 1.572 0 2.57-1.415 2.57-3.643zm-7.177.704c0-1.197.54-1.907 1.456-1.907.93 0 1.524.738 1.524 1.907S8.308 9.84 7.371 9.84c-.895 0-1.442-.725-1.442-1.914z"></path>
+                </svg>
+                <input class="input-field" type="email" placeholder="Entrez votre email ..." id="email" name="email" required>
+            </div>
+            <div class="field">
+                <svg class="input-icon" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+                <path d="M8 1a2 2 0 0 1 2 2v4H6V3a2 2 0 0 1 2-2zm3 6V3a3 3 0 0 0-6 0v4a2 2 0 0 0-2 2v5a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V9a2 2 0 0 0-2-2z"></path>
+                </svg>
+                <input class="input-field" type="password" placeholder="Entrez votre mot de passe ..." id="pass" name="pass" required>
+            </div>
+            <div class="field">
+                <svg class="input-icon" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+                <path d="M8 1a2 2 0 0 1 2 2v4H6V3a2 2 0 0 1 2-2zm3 6V3a3 3 0 0 0-6 0v4a2 2 0 0 0-2 2v5a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V9a2 2 0 0 0-2-2z"></path>
+                </svg>
+                <input type="password" class="input-field" placeholder="Confirmez votre mot de passe ..." id="pass2" name="pass2" required>
+            </div>
+            <div class="btn">
+            <input type="submit" value="S'inscrire" class="button1" name="ok" class="ok">
+            <a href="connexion.php" class="button2">Sign In</a>
+            </div>
+        </form>
 
 <?php
-session_start();
 require "php_mailer/PHPMailerAutoload.php";
 include('bdd.php');
 
@@ -176,29 +187,9 @@ if(isset($_POST['ok'])){
 
 ?>
 
-
-
-
-
-
-
-            <div class="button1">
-                <input type="submit" value="M'inscrire" name="ok" class="ok">
-            </div>
-        </form>
-
-        <div class="reste">
-            <div class="button2">   
-                <p>J'ai déja un compte ?</p>
-                <a href="connexion.php"><button>Se connecter</button></a>
-            </div>
-        </div>
-    </div>
-</section>
-
 </div>
 
-
+</div>
 
 
 
